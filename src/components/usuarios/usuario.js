2bd24c88fbd15888b02f7c1512f1e089c86b084b
Rocco <sadarocco@gmail.com>
2021-08-30 12:30:16 -0300

    add userDetail info

--- src/components/usuarios/usuario.js
+++ src/components/usuarios/usuario.js
@@ -5,6 +5,10 @@ import { Redirect } from "react-router-dom";
 const Usuario = ({ user, setRefresh }) => {
   const [edit, setEdit] = useState(false);
   const [del, setDel] = useState(false);
+  const [detalle, setDetalle] = useState(false)
+  const [userDetail, setUserDetail] = useState([]);
+  const mazos = userDetail.Mazos ? userDetail.Mazos : null;
+  const medallas = userDetail.medallas ? userDetail.medallas : null;
 
   const handleDelete = async (id) => {
     setDel(true);
@@ -20,6 +24,15 @@ const Usuario = ({ user, setRefresh }) => {
   const handleEdit = () => {
     setEdit(true);
   };
+
+  const handleDetalle = async (id) => {
+    setDetalle(true);
+    const response = await fetch(`http://localhost:4000/api/usuario/${id}`, {
+      method: "GET",
+    });
+    const respuesta = await response.json();
+    setUserDetail(respuesta[0]);
+  }
   return (
     <Fragment>
       <tr>
@@ -37,17 +50,18 @@ const Usuario = ({ user, setRefresh }) => {
         <td className="acciones">
           <button
             onClick={handleEdit}
-            className="btn btn-outline-secondary m-1"
+            className="btn btn-outline-secondary"
           >
             Editar
           </button>
           <button
             type="button"
-            className="btn btn-outline-danger font-weight-bold "
+            className="btn btn-outline-danger m-1 "
             onClick={() => handleDelete()}
           >
             Eliminar
           </button>
+          <button onClick={() => handleDetalle(user.id)} type="button" className="btn btn-outline-info">Detalle</button>
         </td>
       </tr>
 
@@ -77,6 +91,46 @@ const Usuario = ({ user, setRefresh }) => {
           </button>
         </ModalFooter>
       </Modal>
+
+      <Modal isOpen={detalle}>
+        <ModalHeader>
+          <h4>Informacion del Usuario</h4>
+        </ModalHeader>
+        <ModalBody>
+          <ul>
+            <li className="text-capitalize ">
+              <b>Nombre: </b> <i>{user.nombre}</i>
+            </li>
+            <li className="text-capitalize">
+              <b>Apellido: </b>
+              <i>{user.apellido}</i>
+            </li>
+            <li>
+              <b>Email: </b>
+              <i>{user.email}</i>
+            </li>
+            <li className="text-capitalize">
+              <b>Mis mazos: </b>
+              <i>{userDetail.Mazos ? mazos.map(mazo => ` ${mazo.nombre}, `):'Cargando mazos...'} </i>
+            </li>
+            <li className="text-capitalize">
+              <b>Mis medallas: </b>
+              <i>{userDetail.medallas ? medallas.map(medalla => ` ${medalla[0].nombre}, `) :'Cargando medallas...'} </i>
+            </li>
+          </ul>
+        </ModalBody>
+        <ModalFooter>
+          <button
+            onClick={() => setDetalle(false)}
+            className="btn btn-block btn-primary"
+            type="button"
+          >
+            OK
+          </button>
+        </ModalFooter>
+      </Modal>
+
+
       {edit ? (
         <Redirect to={{ pathname: "/editarusuario", state: user }} />
       ) : null}
